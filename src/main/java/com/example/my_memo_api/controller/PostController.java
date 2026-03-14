79eda49441f1acb87096d5653839de1120a2abda
TAKEDA HIDEKI <takedahideki8@gmail.com>
2026-03-14 23:49:57 +0900

    Postエンティティに関連するコントローラー、サービス、リポジトリを追加し、初期データを投入するSQLスクリプトを作成_最初のAPI作成

---
+++ src/main/java/com/example/my_memo_api/controller/PostController.java
@@ -0,0 +1,27 @@
+package com.example.my_memo_api.controller;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.my_memo_api.entity.Post;
+import com.example.my_memo_api.service.PostService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/posts")
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    /**
+     * 全ての投稿を取得して返します。
+     */
+    @GetMapping
+    public List<Post> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+}
+
