c6a55c71887b101abfac45d94cd0b4a432c6f5a0
TAKEDA HIDEKI <takedahideki8@gmail.com>
2026-03-14 21:17:14 +0900

    Postエンティティクラスを作成し、データベースのpostsテーブルにマッピング

---
+++ src/main/java/com/example/my_memo_api/entity/Post.java
@@ -0,0 +1,23 @@
+package com.example.my_memo_api.entity;
+
+import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
